9954a3da9e350c8739aceebbac81f89d6ee5616e
Andrea Censi <acensi@ethz.ch>
2018-10-06 09:46:20 +0200

    refresh

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 import logging
-import subprocess
 import os
 import random
+import subprocess
 
-from duckietown_challenges import wrap_evaluator, ChallengeEvaluator, InvalidSubmission
+from duckietown_challenges import wrap_evaluator, ChallengeEvaluator
 
 logging.basicConfig()
 logger = logging.getLogger('evaluator')
 logger.setLevel(logging.DEBUG)
 
-EPISODES = 10
-HORIZON = 500
 
 # we are in Evaluation Container
 class GymEvaluator(ChallengeEvaluator):
@@ -26,9 +24,13 @@ class GymEvaluator(ChallengeEvaluator):
     # start_simulator_process(nsteps, nepisodes, log_output='ros_output.bag')
     # now there is an environment listening on the socket
     def prepare(self, cie):
+        EPISODES = int(os.environ.get('DTG_EPISODES'))  # 10
+        HORIZON = int(os.environ.get('DTG_HORIZON'))  # 500
+        ENVIRONMENT = os.environ.get('DTG_ENVIRONMENT')  # 'Duckietown-Lf-Lfv-Navv-Silent-v0'
+
         # parameters for the submission
         cie.set_challenge_parameters({
-            'env': 'Duckietown-Lf-Lfv-Navv-Silent-v0',
+            'env': ENVIRONMENT,
             'episodes': EPISODES,
             'horizon': HORIZON
         })
@@ -38,13 +40,13 @@ class GymEvaluator(ChallengeEvaluator):
         environment = os.environ.copy()
         environment['DUCKIETOWN_DOMAIN_RAND'] = 'false'
         environment['DUCKIETOWN_MAX_STEPS'] = str(EPISODES * HORIZON)  # TODO: verify this actually controls the MAX
-        environment['DUCKIETOWN_CHALLENGE'] = 'LF'  # TODO: will the challenge define the map?
+        logger.info('challenge: %s' % os.environ['DUCKIETOWN_CHALLENGE'])  #
 
         # this command is on the base image gym-duckietown-server
         cmd = ['launch-gym-server-with-xvfb']
         self.gym_process = subprocess.Popen(
-            args=cmd,
-            env=environment
+                args=cmd,
+                env=environment
         )
         logger.debug('gym launched with pid = {}'.format(self.gym_process.pid))
 
